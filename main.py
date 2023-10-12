236c71dad6669a58670794a29d070058cf8f0b12
Ivan <ivan.kustarve@gmail.com>
2023-10-12 16:45:49 +0300

    ericks

--- main.py
+++ main.py
@@ -1,59 +1,57 @@
 import math
 
-N = 74701165267919
-e = 3145553
+
+def ferma(N, e, C):
+    message = ""
+
+    # выбираем S
+    s = int(math.sqrt(N) // 1)
+
+    # ищем целый корень T
+    i = 0
+    while True:
+        i += 1
+        t = s + i
+        print("t(" + str(i) + ")=" + str(t))
+        w = t ** 2 - N
+        print("w(" + str(i) + ")=" + str(w))
+        w_sqrt = math.sqrt(w)
+        print("w_sqrt(" + str(i) + ")=" + str(w_sqrt))
+        if math.sqrt(w) % 1 == 0:
+            break
+
+    p = t + w_sqrt
+    print("p=" + str(p))
+    q = t - w_sqrt
+    print("q=" + str(q))
+    phi_N = int((p - 1) * (q - 1))
+    print("phi(N)=" + str(phi_N))
+    d = pow(e, -1, phi_N)
+    print("d=" + str(d))
+
+    # дешефруем rsa полученной секретной комбинацией
+    for w in C.split():
+        m = pow(int(w), d, N)
+        sub_str = m.to_bytes(4, byteorder='big').decode('cp1251')
+        message += sub_str
+    print(f"message = {message}")
+
+
+N = 81177745546021
+e = 2711039
 C = '''
-32035658541536
-35242897170964
-6268303368709
-6877322610982
-16329207109754
-35007623593376
-26715311593240
-36220800128563
-25019660581036
-61639733671958
-21186453949445
-72477207535811
+61553353723258
+11339642237403
+55951185642146
+38561524032018
+34517298669793
+33641624424571
+78428225355946
+50176820404544
+68017840453091
+5507834749606
+26675763943141
+47457759065088
 '''
 
-# выбираем стартовое значение A
-if math.sqrt(N) % 1 != 0:
-    a = int(math.sqrt(N) // 1 + 1)
-else:
-    a = int(math.sqrt(N) // 1)
-
-# итерируемся по возможным значениям в поисках целого корня B
-i = 0
-while True:
-    t = a + i
-    print("t" + str(i) + "=" + str(t))
-    b = t ** 2 - N
-    print("b" + str(i) + "=" + str(b))
-    b_sqrt = math.sqrt(b)
-    print("b_sqrt" + str(i) + "=" + str(b_sqrt))
-    i += 1
-    if b_sqrt % 1 == 0:
-        break
-    if i == 100:
-        raise Exception("Не найден целый корень")
-
-# получили p и q на основе A и B
-p = t + b_sqrt
-q = t - b_sqrt
-phi = int((p - 1) * (q - 1))
-d = pow(e, -1, phi)
-
-print("p=" + str(p))
-print("q=" + str(q))
-print("phi(N)=" + str(phi))
-print("d=" + str(d))
-
-# дешефруем rsa полученной секретной комбинацией
-res = ""
-for b in C.split():
-    m = pow(int(b), d, N)
-    sub_str_bytes = m.to_bytes(4, byteorder='big')
-    sub_str = sub_str_bytes.decode('cp1251')
-    res += sub_str
-print(f"res = {res}")
+ferma(N, e, C)
